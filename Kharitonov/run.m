0b6389ef09282cc20395d95caa6d24dbefc2f8e5
YuryStrelkov <ghost_strelkov@mail.ru>
2019-06-25 18:33:58 +0400

    +

--- Kharitonov/run.m
+++ Kharitonov/run.m
@@ -1,6 +1,6 @@
 clear all; close all; clc;
 a = 0.005;
-
+addpath([cd,'\figureSaveExample'])
 % delta=40*10^-6;
 
 delta=2*10^-5;
@@ -18,21 +18,32 @@ pixels = floor(a/delta);
 x=linspace(-a/2,a/2,pixels);
 
 sigma=a/6;
-figure(1)
 
-[ w ] = W_func_distrebution( x,x,256,256, sigma, waveLength, focalLength );
+[ w ] = W_func_distrebution( x,x,64,64, sigma, waveLength, focalLength );
 %  w  =lam/2/cos(theta)*Bi(mod(Phi(x*cos(theta),x,1,lam),2*pi));
 %  w  = Bi(mod(Phi(x*cos(theta),x,1,lam),2*pi));
 
 % w=mod(Phi(x*cos(theta),x,1,lam),2*pi);
 %  w=Phi(x*cos(theta),x,1,lam);
-imagesc(x,x,w.*conj(w));
+
+FIG_HANDLER = figure('Units', 'centimeters', 'OuterPosition', [0 0 17 17]./[0.13 0.11 0.775 0.79991]);
+    
+set(FIG_HANDLER,'DefaultAxesFontSize',14,'DefaultAxesFontName','Times New Roman');
+
+set(FIG_HANDLER,'DefaultTextFontSize',14,'DefaultTextFontName','Times New Roman'); 
+
+imagesc(x*1000,x*1000,w.*conj(w));
 hold on
 t=linspace(0,2*pi,100);
-plot(cos(t)*fclSptSize,sin(t)*fclSptSize,'r');
+plot(cos(t)*fclSptSize*1000,sin(t)*fclSptSize*1000,'r');
 
 hold off;
 colorbar;
 colormap gray;
-% grid on;
 axis equal;
+ylabel('y,\mum');
+xlabel('x,\mum');
+ylim([min(x) max(x)]*1000);
+xlim([min(x) max(x)]*1000);
+redot();
+% plot2svg('Intence(Kirhgoff_integral).svg');
